a23a518ca994fd38dd9e34073191f4d997d4fbb0
acatzk <joshuaimalay@gmail.com>
2023-09-01 16:08:46 +0800

    MM-[45] - [FE] Fix User Cascade delete to following

---
+++ api/prisma/migrations/20230901080552_add_follow_cascading/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_followerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_followingId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
